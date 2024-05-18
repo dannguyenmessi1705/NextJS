9f413152ea03a2fd0d5e0d6a6e220342641c7314
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-19 02:11:42 +0700

    docs: Add font in Next.js

--- the-oasis-next/app/layout.js
+++ the-oasis-next/app/layout.js
@@ -1,6 +1,11 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({
+  subsets: ["latin", "vietnamese"],
+  display: "fallback",
+});
 export const metadata = {
   title: {
     template: "%s | The Wild Oasis",
@@ -12,7 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950 text-primary-100">
+      <body
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+      >
         <header>
           <Logo />
           <Navigation />
