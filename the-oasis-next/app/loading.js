336affe613631abb41a68256bb99f1fc43f34de6
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-19 01:27:29 +0700

    initial new project

---
+++ the-oasis-next/app/loading.js
@@ -0,0 +1,5 @@
+function loading() {
+  return <h1>Fetching data....</h1>;
+}
+
+export default loading;
---
+++ the-oasis-next/app/page.js
@@ -0,0 +1,9 @@
+import Navigation from "./_components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
