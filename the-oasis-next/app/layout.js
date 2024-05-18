21ad89c184230d356eb625a5313d334797b042bf
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-19 02:22:08 +0700

    ui: improve css style navigation

--- the-oasis-next/app/layout.js
+++ the-oasis-next/app/layout.js
@@ -1,5 +1,4 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
 const josefin = Josefin_Sans({
@@ -18,14 +17,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100 flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>The Copyright by Di Dan</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
