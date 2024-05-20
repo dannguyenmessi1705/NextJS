3389f1de21af392fc516e68255d1bae66a09fea9
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-21 01:24:32 +0700

    feat: highlight side nav

--- the-oasis-next/app/cabins/[cabinId]/page.js
+++ the-oasis-next/app/cabins/[cabinId]/page.js
@@ -2,6 +2,8 @@ import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+import TextExpander from "@/app/_components/TextExpander";
+
 // Tạo metadata cho route động
 export async function generateMetadata({ params }) {
   const cabin = await getCabin(params.cabinId);
@@ -40,7 +42,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
