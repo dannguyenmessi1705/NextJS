6a9f6cb3c1f77e6a1c7e5d815022861deba6a548
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-05-23 03:16:21 +0700

    docs + feat: useTransition to display loading when exec server action in event handle

--- the-oasis-next/app/_components/DeleteReservation.js
+++ the-oasis-next/app/_components/DeleteReservation.js
@@ -1,12 +1,32 @@
 "use client";
-import { deleteReservation } from "@/app/_lib/serverAction"
-import { TrashIcon } from '@heroicons/react/24/solid';
+import { deleteReservation } from "@/app/_lib/serverAction";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
+  const [isPending, startTransition] = useTransition();
+  const handleDelete = () => {
+    if (confirm("Are you delete this items")) {
+      startTransition(() => deleteReservation(bookingId)); // Nếu xác nhận xóa thì gọi hàm deleteReservation và bắt đầu transition (isPending = true)
+    }
+  };
   return (
-    <button onClick={() => deleteReservation(bookingId)} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
+    <button
+      onClick={handleDelete}
+      disabled={isPending}
+      className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+    >
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
